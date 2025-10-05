1e380fbad30790ca51054b037d160c32cbfe9205
moali123456 <moali.sadeq86@gmail.com>
2025-10-06 00:41:24 +0300

    comments section

--- src/app/[locale]/home/comments-section.jsx
+++ src/app/[locale]/home/comments-section.jsx
@@ -1,5 +1,4 @@
-
-import { RatingComponent } from "@/app/_components/product-rating/product-rating";
+import React from "react";
 import {
   Carousel,
   CarouselContent,
@@ -10,14 +9,20 @@ import {
 import { useTranslations, useLocale } from "next-intl";
 import Images from "../../../../public/assets/images/images";
 import { getComments } from "@/lib/actions/comments/comments-actions";
+import { RatingComponent } from "@/app/_components/product-rating/product-rating";
 
 export default async function CommentsSection() {
+  // Fetch Categories
+  const commentsData = await getComments();
+
+  return <CommentsContent commentsData={commentsData} />;
+}
+
+// Client component to handle translations
+function CommentsContent({ commentsData }) {
   const t = useTranslations();
   const locale = useLocale();
   const isRTL = locale === "ar";
-
-  // Fetch Categories
-  const productsData = await getComments();
 
   return (
     <div className="container_bx bg-[#F2F2F2] py-8">
@@ -26,28 +31,6 @@ export default async function CommentsSection() {
           {t("client-comments")}
         </h1>
       </div>
-      {/* <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full"
-      >
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel> */}
 
       <Carousel
         opts={{
@@ -56,74 +39,56 @@ export default async function CommentsSection() {
         className="w-full"
       >
         <CarouselContent>
-          <CarouselItem key={1} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <div className="p-6 bg-white rounded-sm shadow-xs">
-                <div className="">
-                  <img
-                    src={Images.commentIcon}
-                    alt="pic"
-                    className="mb-4 size-5"
-                  />
+          {commentsData && commentsData.length > 0 ? (
+            commentsData.map((comment) => (
+              <CarouselItem
+                key={comment?.id}
+                className="md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="p-1">
+                  <div className="p-6 bg-white rounded-sm shadow-xs">
+                    <div className="">
+                      <img
+                        src={Images.commentIcon}
+                        alt="pic"
+                        className="mb-4 size-5"
+                      />
 
-                  <p className="text-gray-700 text-sm line-clamp-4">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                    Ducimus necessitatibus possimus id accusamus fugiat qui ex
-                    dolorem suscipit asperiores recusandae! Aliquam repudiandae
-                    nisi molestiae qui, sit porro distinctio perspiciatis saepe.
-                  </p>
+                      <p className="text-gray-700 text-sm line-clamp-4">
+                        {comment?.body}
+                        {/* Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Ducimus necessitatibus possimus id accusamus
+                        fugiat qui ex dolorem suscipit asperiores recusandae!
+                        Aliquam repudiandae nisi molestiae qui, sit porro
+                        distinctio perspiciatis saepe. */}
+                      </p>
 
-                  <div className="flex justify-between items-center gap-4 mt-5">
-                    <div className="flex gap-2 items-center">
-                      <span className="size-14 bg-gray-200 rounded-full"></span>
-                      <div className="flex flex-col">
-                        <h1 className="text-gray-900 text-sm font-bold capitalize">
-                          user name
-                        </h1>
-                        <h3 className="text-xs text-gray-400">title</h3>
+                      <div className="flex justify-between items-center gap-4 mt-5">
+                        <div className="flex gap-2 items-center">
+                          <span className="size-14 bg-gray-200 rounded-full"></span>
+                          <div className="flex flex-col">
+                            <h1 className="text-gray-900 text-sm font-bold capitalize">
+                              {comment?.user?.fullName}
+                            </h1>
+                            <h3 className="text-xs text-gray-400">title</h3>
+                          </div>
+                        </div>
+
+                        {/* Rating */}
+                        <RatingComponent
+                          value={comment?.likes}
+                          readonly={true}
+                          showLabel={true}
+                        />
                       </div>
                     </div>
-
-                    {/* Rating */}
-                    <RatingComponent
-                      value={4}
-                      readonly={true}
-                      showLabel={true}
-                    />
                   </div>
                 </div>
-              </div>
-              {/* <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6 border-0">
-                  <span className="text-3xl font-semibold">1</span>
-                </CardContent>
-              </Card> */}
-            </div>
-          </CarouselItem>
-
-          <CarouselItem key={2} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <div className="p-6 bg-white rounded-sm shadow-xs">2</div>
-            </div>
-          </CarouselItem>
-
-          <CarouselItem key={3} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <div className="p-6 bg-white rounded-sm shadow-xs">3</div>
-            </div>
-          </CarouselItem>
-
-          <CarouselItem key={4} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <div className="p-6 bg-white rounded-sm shadow-xs">4</div>
-            </div>
-          </CarouselItem>
-
-          <CarouselItem key={5} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <div className="p-6 bg-white rounded-sm shadow-xs">5</div>
-            </div>
-          </CarouselItem>
+              </CarouselItem>
+            ))
+          ) : (
+            <p className="text-gray-500">No products available.</p>
+          )}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
@@ -131,3 +96,4 @@ export default async function CommentsSection() {
     </div>
   );
 }
+
